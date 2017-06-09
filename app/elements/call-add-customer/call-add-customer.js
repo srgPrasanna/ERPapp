e6294a98b621643a672019b69665ac568bda0f10
srgPrasanna <srg.prasanna@gmail.com>
2017-06-09 11:57:57 +0530

    billing and shipping element updated in call-add-customer

--- app/elements/call-add-customer/call-add-customer.js
+++ app/elements/call-add-customer/call-add-customer.js
@@ -1,6 +1,10 @@
+var item1;
+var supplier_name="";
+var supplier_id="";
+var tablename;
+var customerid;
 Polymer({
   is:"call-add-customer",
-
   ready:function(){
     this.colvar=true;
   },
@@ -16,14 +20,14 @@ Polymer({
   }
   },
   searchitemdet:function(){
-    if(this.SupNameval!=""){
+    if(this.value1!=""){
     document.querySelector("#anchorID").style.display="none";
      document.querySelector('#additemid').disabled=false;
-    var obj={supname:this.SupNameval};
+    var obj={supname:this.value1};
     document.querySelector("call-add-customer-ironajax").searchsidFN(obj);
   }
   else {
-    alert("please enter valid customername");
+    alert("please enter valid customer_name");
   }
   },
   additemFn:function(){
@@ -38,10 +42,10 @@ Polymer({
   },
   submitFn:function(data){
     document.querySelector('#additemid').disabled=false;
-    alert(data);
+    customerid=data;
     var postvalue ={
                     "sidval":data,
-                    "snameval":this.SupNameval,
+                    "snameval":this.value1,
                     "adval1"  :this.Add1val,
                     "adval2"  :this.Add2val,
                     "adval3"  :this.Add3val,
@@ -54,24 +58,26 @@ Polymer({
                     "emidval"  :this.EmIDval,
                     "statusval":"created"
                   };
-                  this.cuid=this.SupIdval;
+                  this.cuid=this.value1;
           document.querySelector("call-add-customer-ironajax").to_ironajax(postvalue);
           },
           datafetchFn:function(data){
-            this.SupIdval=data[0].customerid;
-            this.SupNameval=data[0].customername;
-            this.Add1val=data[0].address1;
-            this.Add2val=data[0].address2;
-            this.Add3val=data[0].address3;
+            alert(JSON.stringify(data));
+            customerid=data[0].customer_id;
+            this.value1=data[0].customer_id;
+            this.value1=data[0].customer_name;
+            this.Add1val=data[0].address_1;
+            this.Add2val=data[0].address_2;
+            this.Add3val=data[0].address_3;
             this.selection=data[0].country;
             this.selectionstate=data[0].state;
             this.Citynameval=data[0].city;
             this.Pinval=data[0].pincode;
-            this.MobNumberval1=data[0].mobile1;
-            this.MobNumberval2=data[0].mobile2;
+            this.MobNumberval1=data[0].mobile_1;
+            this.MobNumberval2=data[0].mobile_2;
             this.EmIDval=data[0].email;
             this.staval=data[0].status;
-            this.cuid=this.SupIdval;
+            this.cuid=this.value1;
           },
           getironpageFn:function(data){
             this.page="item-display";
@@ -107,5 +113,146 @@ Polymer({
   },
   getstatejsondata:function(getData){
     this.stateitems=getData;
-  }
+  },
+  FnSearchEnquiry1:function(e){
+       if(e.keyCode==13|| e.keyCode==40)
+       this.querySelector('#transportinput3').focus();
+       var arr=[];
+       arr.push({"itemdes1":"-----Select-----"});
+       this.querySelector('#transportinput3').style.visibility='visible';
+       if(e.keyCode==8){
+         this.itemflag1="true";
+         this.itemval1="";
+         var len=(this.value1).length;
+         if(len<=1){
+           this.querySelector('#transportinput3').style.visibility='hidden';
+           this.itemArray1="";
+           this.itemval1="";
+         }
+         if(len>1){
+           this.querySelector('#transportinput3').style.visibility='visible';
+           var backsubval=(((this.value1).substring(0,(len-1))).trim()).toUpperCase();
+           for(var i=0;i<item1.length;i++)
+           {
+             var subval=((item1[i].customer_name).trim()).substring(0,backsubval.length);
+             if((item1[i].customer_name).toUpperCase().indexOf((this.value1).toUpperCase())!=-1)
+             {
+               var obj1={"itemdes1":"","enquiry_no":""};
+               obj1.itemdes1=item1[i].customer_name;
+               obj1.customer_id=item1[i].customer_id;
+               arr.push(obj1);
+             }
+           }
+           this.itemArray1=arr;
+         }
+       }
+       if(e.keyCode!=8&& e.keyCode!=16&& e.keyCode!=13 && e.keyCode!=38&&e.keyCode!=40&&e.keyCode!=37&&e.keyCode!=39){
+         if(this.itemflag1=="true") {
+           this.itemval1 = (this.value1).toUpperCase()+String.fromCharCode((e.keyCode)).toUpperCase();
+           this.itemflag1="false";
+         }
+         else
+         this.itemval1 = this.value1 +String.fromCharCode((e.keyCode));
+         if(this.itemval1.length>0)
+         {
+           for(var i=0;i<item1.length;i++)
+           {
+             var subval=((item1[i].customer_name).trim()).substring(0,this.itemval1.length);
+             if((item1[i].customer_name).toUpperCase().indexOf((this.itemval1).toUpperCase())!=-1)
+             {
+               var obj1={"itemdes1":"","enquiry_no":""};
+               obj1.itemdes1=item1[i].customer_name;
+               obj1.customer_id=item1[i].customer_id;
+               arr.push(obj1);
+             }
+           }
+           if(arr.length>0)
+             this.itemArray1=arr;
+           else
+           {
+             var obj1={"itemdes1":"","enquiry_no":""};
+             obj.itemdes1="No items found";
+             arr.push(obj1);
+             this.itemArray1=arr;
+           }
+         }
+       }
+
+
+       },
+       //customer_id
+
+       FnSelectEnquiry2:function(e){
+       this.querySelector('#transportinput3').style.visibility='hidden';
+       supplier_name = e.target.selectedItem.textContent.trim();
+       supplier_id = e.target.selectedItem.value.trim();
+       this.itemArray1=[];
+       document.querySelector('#transportinput3').selected=-1;
+       this.value1=supplier_name;
+     },
+     autocompletearr:function(e)
+    {
+      alert(JSON.stringify(e.detail.response.returnval));
+      item1=e.detail.response.returnval;
+    },
+    check:function(data){
+      tablename=data;
+    },
+    addBillShipAddress:function(){
+      this.page="billing";
+      document.querySelector('#billinput').value=this.value1;
+    },
+    saveBillShipAddress:function(){
+      var billjsonobj={};
+      var shipjsonobj={};
+      var billcountry=document.querySelector('#billaddressid').ccode;
+      var billstate=document.querySelector('#billaddressid').scode;
+      var billcity=document.querySelector('#billaddressid').cityname;
+      var billstreet=document.querySelector('#billaddressid').streetname;
+      var billpincodeno=document.querySelector('#billaddressid').pincode;
+      billjsonobj.customerid=customerid;
+      billjsonobj.billcountry=billcountry;
+      billjsonobj.billstate=billstate;
+      billjsonobj.billcity=billcity;
+      billjsonobj.billstreet=billstreet;
+      billjsonobj.billpincodeno=billpincodeno;
+      var shipcountry=document.querySelector('#shipaddressid').ccode;
+      var shipstate=document.querySelector('#shipaddressid').scode;
+      var shipcity=document.querySelector('#shipaddressid').cityname;
+      var shipstreet=document.querySelector('#shipaddressid').streetname;
+      var shippincodeno=document.querySelector('#shipaddressid').pincode;
+      shipjsonobj.shipcountry=shipcountry;
+      shipjsonobj.shipstate=shipstate;
+      shipjsonobj.shipcity=shipcity;
+      shipjsonobj.shipstreet=shipstreet;
+      shipjsonobj.shippincodeno=shippincodeno;
+      if(this.checkvalue==true){
+        shipjsonobj={shipcountry:billjsonobj.billcountry,shipstate:billjsonobj.billstate,shipcity:billjsonobj.billcity,shipstreet:billjsonobj.billstreet,shippincodeno:billjsonobj.billpincodeno};
+      }
+      else{
+
+      }
+      document.querySelector('call-add-customer-ironajax').bindBilldata(billjsonobj);
+      document.querySelector('call-add-customer-ironajax').bindShipdata(shipjsonobj);
+    },
+    newBillShipAddress:function(){
+      document.querySelector('#billaddressid').cityname="";
+      document.querySelector('#billaddressid').streetname="";
+      document.querySelector('#billaddressid').pincode="";
+      document.querySelector('#shipaddressid').cityname="";
+      document.querySelector('#shipaddressid').streetname="";
+      document.querySelector('#shipaddressid').pincode="";
+    },
+    fadeShip:function(){
+      if(this.checkvalue==true){
+        // $('#shiptag').fadeOut('slow',.6);
+        $('#shiptag').fadeOut();
+        // $('#shiptag').append('<div style="position: absolute;top:0;left:0;width: 100%;height:100%;z-index:2;opacity:0.4;filter: alpha(opacity = 50)"></div>');
+      }
+      else{
+        // $('#shiptag').fadeIn('slow',.6);
+        $('#shiptag').fadeIn();
+        // $('#shiptag').append('<div style="position: absolute;top:0;left:0;width: 100%;height:100%;z-index:2;opacity:0.4;filter: alpha(opacity = 50)"></div>');
+      }
+    }
 });
